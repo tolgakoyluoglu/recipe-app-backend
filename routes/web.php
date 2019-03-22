6c063c7ec0af2bd0728dd54ad63069b22fae3920
Tolga Köyluoglu <tolga.koyluoglu@chasacademy.se>
2019-03-23 00:53:11 +0100

    Add adminer, fixed oath with passport, works to register/login via postman

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/Route::any('adminer', '\Miroc\LaravelAdminer\AdminerAutologinController@index');
+
 
 Route::get('/', function () {
     return view('welcome');
